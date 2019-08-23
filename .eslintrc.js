295a59b04ff963cb76bb3b6cbf1cddfab465bce2
Alexey Alkhutov <a.alkhutov@gmail.com>
2019-08-23 15:38:15 +0300

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,22 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
   rules: {
     'no-use-before-define': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'no-param-reassign': 'off',
-    'camelcase': 'off',
+    camelcase: 'off',
     'no-continue': 'off',
     'no-case-declarations': 'off',
     'consistent-return': 'off',
@@ -30,5 +28,29 @@ module.exports = {
     'no-restricted-globals': 'off',
     'no-prototype-builtins': 'off',
     'class-methods-use-this': 'off',
-  },
+    'no-throw-literal': 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never'
+      }
+    ],
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: false,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false
+        }
+      }
+    ]
+  }
 };
